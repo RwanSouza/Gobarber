2c94dda5fede41013826d5f7422bf3610d965242
Rwan Souza <rwan.souza@hotmail.com>
2020-04-23 17:29:24 -0300

    Listagem de agendamentos

---
+++ src/app/controllers/rota.js
@@ -0,0 +1,18 @@
+import User from '../models/User';
+import Appointment from '../models/Appointment';
+class rota {
+  async index(req, res){
+    const checkUserProvider = await User.findOne({
+      where: { id: req.userId, provider: true }
+    });
+
+    if (!checkUserProvider){
+      return res.status(401).json({ error: 'User is not a provider' })
+    }
+
+    const { date } = req.query;
+    return res.json({ date });
+  }
+}
+
+export default new rota();
