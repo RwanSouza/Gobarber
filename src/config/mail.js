4e861c98a3cc81c8cd36aca89f89c9683c323536
Rwan Souza <rwan.souza@hotmail.com>
2020-04-28 20:32:04 -0300

    Enviado de email e templates de email

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'ffac07b442391d',
+    pass: '12119be48ad1f7',
+  },
+  default: {
+    from: 'Equipe Gobarber <noreply@gobarber.com>'
+  },
+};
